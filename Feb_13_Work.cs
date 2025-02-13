3d2ecb2ca0fb259b792b09c221d7a0f2ab521153
theblindmatt69 <theblindmatt@gmail.com>
2025-02-13 10:41:07 -0700

    Create Feb_13_Work.cs
    learning how to write in c#

---
+++ Feb_13_Work.cs
@@ -0,0 +1,9 @@
+//today i learnt how to write lines of text in c# shown below
+Console.WriteLine("hello world");
+Console.Write("my name is...");
+Console.Write("what!...");
+Console.Write("my name is...");
+Console.Write("what!...");
+Console.Write("my name is. chiki chiki..");
+Console.Write("SLIM SHADY!...");
+//this is how to be eminem in c#
