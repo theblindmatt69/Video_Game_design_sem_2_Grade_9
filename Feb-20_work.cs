c1872d923f11878d5d3f4a6bce2f31266b7f5b87
theblindmatt69 <theblindmatt@gmail.com>
2025-02-20 10:46:58 -0700

    Update and rename Feb-20 to Feb-20_work.cs

---
+++ Feb-20_work.cs
@@ -0,0 +1,10 @@
+// today we went over unit 4 where we learned how to add and program integers
+string firstName; //integer create
+firstName = "Bob"; // integer define
+Console.WriteLine(firstName); // integer run/ write
+firstName = "Liem";
+Console.WriteLine(firstName);
+firstName = "Isabella";
+Console.WriteLine(firstName);
+firstName = "Yasmin";
+Console.WriteLine(firstName);
