f6f966f1bd0e202411ffa4225c3960038c75841d
theblindmatt69 <theblindmatt@gmail.com>
2025-03-06 10:27:44 -0700

    Create Mar_6.cs

---
+++ Mar_6.cs
@@ -0,0 +1,16 @@
+//HAPPY GHANNA BIRTHDAY, GHANNA IS DA BEST, GHANNA IS GUD AT SOCCER
+//today we learned about printing litteral values
+
+
+//Float Type    Precision
+----------------------------
+float         ~6-9 digits
+double        ~15-17 digits
+decimal        28-29 digits
+//above is info that i want to remember
+//code:
+Console.WriteLine("123");
+Console.WriteLine(123);
+
+Console.WriteLine("true");
+Console.WriteLine(true);
