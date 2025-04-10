27d5793c7d97e5e38f6252a76a15877995f9345d
theblindmatt69 <theblindmatt@gmail.com>
2025-04-10 10:06:41 -0600

    Create April_10.cs
    module 5 unit 1-2

---
+++ April_10.cs
@@ -0,0 +1,4 @@
+//today we learned how to add integers together along with incliding strings into a single line with the integers
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
